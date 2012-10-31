0e26b10528a4080dc9d74baf27736b2f063ecbf6
Trey Hunner <treyhunner@gmail.com>
2012-10-30 19:57:02 -0700

    Add contact page

--- eatwell/urls.py
+++ eatwell/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'eatwell.views.home', name='home'),
+     url(r'^contact$', 'eatwell.views.contact', name='contact'),
     # url(r'^eatwell/', include('eatwell.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
