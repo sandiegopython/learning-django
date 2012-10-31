f1030e593593927d79d570bd85ae2913312c0f56
Trey Hunner <treyhunner@gmail.com>
2012-10-30 19:58:46 -0700

    Add link to contact page and link back to homepage

--- eatwell/views.py
+++ eatwell/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     """A simple page containing just "Hello world!\""""
-    return HttpResponse("Hello world!")
+    return HttpResponse("Hello world!" '<a href="/contact">Contact us</a>')
 
 
 def contact(request):
     """Page stating our contact information"""
-    return HttpResponse("Contact information: 888.888.8888")
+    return HttpResponse("Contact information: 888.888.8888"
+                        '<a href="/">Back home</a>')
