159e47fdc40f67f1a9ee9b6bec888cb516823977
Trey Hunner <treyhunner@gmail.com>
2012-11-02 22:52:34 -0700

    Add page to display list of restaurants
    See documentation on [making queries][1]
    
    [1]: https://docs.djangoproject.com/en/1.4/topics/db/queries/

--- eatwell/views.py
+++ eatwell/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from restaurants.models import Restaurant
+
 
 class Home(TemplateView):
 
@@ -23,3 +25,18 @@ class Contact(TemplateView):
         return {'title': "Contact Us"}
 
 contact = Contact.as_view()
+
+
+class RestaurantListView(TemplateView):
+
+    """Page to list all restaurants"""
+
+    template_name = 'restaurant_list.html'
+
+    def get_context_data(self):
+        return {
+            'title': "Restaurants",
+            'restaurants': Restaurant.objects.all(),
+        }
+
+restaurant_list = RestaurantListView.as_view()
