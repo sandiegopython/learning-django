fb13628491b2f15aa79c200ec508e3933557a381
Trey Hunner <treyhunner@gmail.com>
2012-11-02 22:39:59 -0700

    Allow all fields to be blank except `name`
    See documentation for [`Field.blank` option][1].
    
    [1]: https://docs.djangoproject.com/en/1.4/ref/models/fields/#django.db.models.Field.blank

--- eatwell/restaurants/models.py
+++ eatwell/restaurants/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=300)
-    address1 = models.CharField(max_length=50)
-    address2 = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-    zip_code = models.CharField(max_length=10)
+    address1 = models.CharField(max_length=50, blank=True)
+    address2 = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    zip_code = models.CharField(max_length=10, blank=True)
 
     def __unicode__(self):
         return self.name
