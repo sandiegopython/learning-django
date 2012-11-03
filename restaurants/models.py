5e99b684a284368261a648d0e96f7fc9bc4c944d
Trey Hunner <treyhunner@gmail.com>
2012-11-02 23:00:36 -0700

    Abstract template logic into address model method
    Business logic doesn't belong in templates.  This method is easier to
    read and more easily reusable (see [Don't Repeat Yourself][1]).
    
    See documentation on built-in [linebreaksbr][2] template filter.  For
    information on why the address method is called when used as a template
    variable see [rendering a context][3] in the template documentation.
    
    [1]: http://c2.com/cgi/wiki?DontRepeatYourself
    [2]: https://docs.djangoproject.com/en/1.4/ref/templates/builtins/#std:templatefilter-linebreaksbr
    [3]: https://docs.djangoproject.com/en/1.4/ref/templates/api/#rendering-a-context
    
    Demonstrates:
    * First filter (linebreaksbr)
    * Principle of keeping tricky logic out of templates

--- restaurants/models.py
+++ restaurants/models.py
@@ -10,5 +10,14 @@ class Restaurant(models.Model):
     state = USStateField(max_length=2, blank=True)
     zip_code = models.CharField(max_length=10, blank=True)
 
+    def address(self):
+        """Return full address of restaurant, formatted nicely"""
+        if not self.city:
+            return ""
+        address = self.address1
+        if self.address2:
+            address += "\n" + self.address2
+        return address + "\n{s.city}, {s.state} {s.zip_code}".format(s=self)
+
     def __unicode__(self):
         return self.name
