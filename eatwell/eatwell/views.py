46447fbe9cfb2f2d9265d1a631cd540c8171d4d6
Trey Hunner <treyhunner@gmail.com>
2012-11-02 21:31:16 -0700

    Add contact page

--- eatwell/eatwell/views.py
+++ eatwell/eatwell/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 def home(request):
     """A simple page containing just "Hello world!\""""
     return HttpResponse("Hello world!")
+
+
+def contact(request):
+    """Page stating our contact information"""
+    return HttpResponse("Contact information: 888.888.8888")
