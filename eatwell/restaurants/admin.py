1d44db43b91ef34245ceee03aa87500135b99728
Trey Hunner <treyhunner@gmail.com>
2012-11-02 21:18:42 -0700

    Remove eatwell project

--- eatwell/restaurants/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from models import Restaurant, Review
-
-
-admin.site.register(Restaurant)
-admin.site.register(Review)
--- eatwell/restaurants/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from models import Review
-
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = ('title', 'body')
