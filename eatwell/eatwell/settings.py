c4c855368ff6fcc26d1be63e62c7080524529dcc
Paul Collins <paul.collins@brightscope.com>
2013-02-19 22:29:20 -0800

    Add static files and bootstrap

--- eatwell/eatwell/settings.py
+++ eatwell/eatwell/settings.py
@@ -1,7 +1,10 @@
 # Django settings for eatwell project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -46,18 +49,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '{0}/uploads'.format(BASE_PATH)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/uploads/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '{}/static_media'.format(BASE_PATH)
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -65,6 +68,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+        '{}/global_static'.format(BASE_PATH),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -105,6 +109,7 @@ ROOT_URLCONF = 'eatwell.urls'
 WSGI_APPLICATION = 'eatwell.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '{}/templates'.format(BASE_PATH),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- eatwell/eatwell/urls.py
+++ eatwell/eatwell/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,3 +11,6 @@ urlpatterns = patterns('',
      url(r'^restaurants/', include('restaurants.urls')),
      url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
